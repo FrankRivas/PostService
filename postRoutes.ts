038c500c2907656cfe15cf2e0db3147393bd7dc7
FrankRivas <cfrivas2016@gmail.com>
2019-12-05 11:58:43 -0600

    add individual routes files

---
+++ postRoutes.ts
@@ -0,0 +1,38 @@
+import * as http from 'http'
+import { getPosts, getPost, deletePost, updatePost, createPost } from './posts'
+import { obtatainDataCode } from './errorCodes'
+
+export async function routePosts(
+	req: http.IncomingMessage,
+	res: http.ServerResponse
+) {
+	let url = req.url as string
+	if (/^\/posts$/.test(url)) {
+		if (req.method === 'GET') {
+			await getPosts(res)
+		} else if (req.method === 'POST') {
+			await createPost(req, res)
+		} else {
+			obtatainDataCode(res, 405)
+		}
+	} else if (url.match(/^\/post\/([0-9]+)$/)) {
+		let param = url.match(/^\/post\/([0-9]+)/)
+		if (req.method === 'GET') {
+			if (param !== null) {
+				await getPost(res, param[1])
+			}
+		} else if (req.method === 'DELETE') {
+			if (param !== null) {
+				await deletePost(res, param[1])
+			}
+		} else if (req.method === 'PUT') {
+			if (param !== null) {
+				await updatePost(req, res, param[1])
+			}
+		} else {
+			obtatainDataCode(res, 405)
+		}
+	} else {
+		obtatainDataCode(res, 405)
+	}
+}
--- routes.ts
+++ routes.ts
@@ -1,39 +1,21 @@
 import * as http from 'http'
-import { getPosts, getPost, deletePost, updatePost, createPost } from './posts'
+import { routePosts } from './postRoutes'
 import { obtatainDataCode } from './errorCodes'
-import { routeComments } from './commentsRoutes'
+import { routeComments } from './commentRoutes'
 
 export async function routing(
 	req: http.IncomingMessage,
 	res: http.ServerResponse
 ) {
 	let url = req.url as string
-	if (/^\/posts$/.test(url)) {
-		if (req.method === 'GET') {
-			await getPosts(res)
-		} else if (req.method === 'POST') {
-			await createPost(req, res)
-		} else {
-			obtatainDataCode(res, 405)
-		}
-	} else if (url.match(/^\/post\/([0-9]+)$/)) {
-		let param = url.match(/^\/post\/([0-9]+)/)
-		if (req.method === 'GET') {
-			if (param !== null) {
-				await getPost(res, param[1])
-			}
-		} else if (req.method === 'DELETE') {
-			if (param !== null) {
-				await deletePost(res, param[1])
-			}
-		} else if (req.method === 'PUT') {
-			if (param !== null) {
-				await updatePost(req, res, param[1])
-			}
-		} else {
-			obtatainDataCode(res, 405)
-		}
-	} else {
+	if (/^\/posts$/.test(url) || url.match(/^\/post\/([0-9]+)$/)) {
+		await routePosts(req, res)
+	} else if (
+		url.match(/^\/post\/([0-9]+)\/comments/) ||
+		url.match(/^\/comment\/([0-9]+)$/)
+	) {
 		await routeComments(req, res)
+	} else {
+		obtatainDataCode(res, 405)
 	}
 }
